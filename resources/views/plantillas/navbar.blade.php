9e03515b3eff7224573fde7037f12ae65b800426
flopez918 <flopez918@gmail.com>
2024-05-30 12:14:54 -0500

    Se modifico el navbar y newPost, cambiado el menu (opcion Nuevo Post) y centrando el boton de publicar

--- resources/views/plantillas/navbar.blade.php
+++ resources/views/plantillas/navbar.blade.php
@@ -6,7 +6,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item"><a class="nav-link" href="{{ url('home')}}">Inicio</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ url('add')}}">Adicionar Post</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ url('add')}}">Nuevo Post</a></li>
                 <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
                 <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Blog</a></li>
                 <form method="POST" action="{{ route('logout') }}">
